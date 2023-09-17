46c090e1df4850838e9d2f00fb108f76a4c83756
shimu-7 <shamima19shimu@gmail.com>
2023-09-17 11:28:56 +0600

    showed toast for exceeding 20credit and selecting same course multiple times

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -3,16 +3,13 @@ const Cart = ({ cart , handleCredit}) => {
     // console.log(cart);
     
      //console.log(handleCredit)
-     const handle= ()=>{
-        console.log(cart);
-     }
     return (
        
         <div>
 
             {
 
-                <div className="w-[325px] px-2 py-3 shadow-lg">
+                <div className="w-[300px] px-2 py-3 shadow-lg">
                     <div >
                     <img className="w-full h-40" src={cart.image} alt="" />
                     </div>
@@ -24,11 +21,11 @@ const Cart = ({ cart , handleCredit}) => {
                         <div className="w-4 h-4">
                         <img  src="https://i.ibb.co/zrvQnCs/Dollar.png" alt="" />
                         </div>
-                        <h3>price: {cart.price}</h3>
+                        <h3>Price: {cart.price}</h3>
                         <div className="w-4 h-4">
                         <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfLTmVrocYDWPLt8XywVWK_ozgTl7N-s0IXA&usqp=CAU" alt="" />
                         </div>
-                        <h3>price: {cart.credit}hr</h3>
+                        <h3>Credit: {cart.credit}hr</h3>
                     </div>
                     <div  className="text-center text-white font-medium mt-3 ">
                     <button className=" w-72 bg-blue-500 rounded-lg" onClick={()=>handleCredit(cart)}>Select</button>
--- src/Components/Carts.jsx
+++ src/Components/Carts.jsx
@@ -1,50 +1,88 @@
+
 import { useEffect, useState } from "react";
+import {  ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Cart from "./Cart";
+
 
 const Carts = () => {
     const [carts, setCarts] = useState([])
-    const [res,setRes]=useState(20);
-    const [credit,setCredit]=useState(0);
-    const [selectedCourse,setSelectedCourse]=useState([]);
+    const [res, setRes] = useState(20);
+    const [credit, setCredit] = useState(0);
+    const [price,setPrice]=useState(0);
+    const [selectedCourse, setSelectedCourse] = useState([]);
     useEffect(() => {
         fetch('course.json')
             .then(res => res.json())
             .then(data => setCarts(data))
     }, []);
+    
 
     // console.log(1);
     // const handleCredit = cart =>{
     //     console.log(cart);
     //     console.log(1);
-       
+
     // }
-    const handleCredit= cart =>{
-        console.log(cart);
-        const newSelected=[...selectedCourse,cart];
+    const ToastNotification1 = () => toast("This course is already selected.", {
+        position: toast.POSITION.RIGHT_TOP,
+        autoClose: 5000});
+        const ToastNotification2 = () => toast("20 Credit exceeded", {
+            position: toast.POSITION.RIGHT_TOP,
+            autoClose: 5000});
+    
+
+    const handleCredit = cart => {
+        // console.log(cart);
+        const newCredit=credit+cart.credit;
+       const check=selectedCourse.filter(course=>course.course_name===cart.course_name)
+       if(check.length===0 && newCredit<=20){
+        const newSelected = [...selectedCourse, cart];
         setSelectedCourse(newSelected);
+        const rem=res-cart.credit
+        setRes(rem);
+        // console.log(res-cart.credit)
+        setCredit(newCredit)
+        setPrice(price+cart.price)
+       }
+       else if(newCredit>20 && check.length>0 ){
+        ToastNotification1(); 
+        ToastNotification2(); 
+       }
+       else if(newCredit>20){
+        ToastNotification2();   
+       }
+       else{
+        // console.log(check);
+        // console.log("already selected")
+        ToastNotification1();       
+       }
     }
     
     return (
         <>
-            <div className="flex mx-5 my-5">
-                <div className="grid grid-cols-3 gap-2">
+            <div className="flex mx-1 my-5">
+                <div className="grid grid-cols-3 gap-5">
                     {
                         carts.map(cart => <Cart key={cart.course_name} handleCredit={handleCredit} cart={cart}></Cart>)
                     }
                 </div>
-                <div className="w-[350px] h-1/3 ml-5 shadow-lg px-4 py-4">
+                <div className="w-[300px] h-1/3 ml-5 shadow-lg px-4 py-4">
                     <h3 className="text-blue-500 font-medium mb-2">Credit Hour Remaining {res} hr</h3>
                     <hr />
                     <h3 className="text-lg font-bold">Course Name</h3>
                     <div className="my-2">
                         <ol type="1">
                             {
-                                selectedCourse.map((course,idx)=><li key={course.course_name}>{idx+1}. {course.course_name}</li>)
+                                selectedCourse.map((course, idx) => <li key={course.course_name}>{idx + 1}. {course.course_name}</li>)
                             }
                         </ol>
+                        <ToastContainer />
                     </div>
                     <hr />
-                    <h3>Total Credit Hour: {credit} </h3>
+                    <h3 className="my-2">Total Credit Hour: {credit} </h3>
+                    <hr />
+                    <h3 className="my-2">Total Price: {price} </h3>
                 </div>
             </div>
         </>
